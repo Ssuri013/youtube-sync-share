786e00e6f9c739c264671d10d9c363c39837bd7f
shubham <shubham013@live.com>
2019-05-26 09:36:10 +0530

    Revert "Revert "Sync server and client""
    This reverts commit 1c2ed563a1c1ec9efe2794b2ac0fc34483dc8bed.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,35 @@ import Player from './components/player.component';
 
 class App extends React.Component {
 
- 
+ constructor() {
+   super();
+   this.state = { userName: "", login: false };
+ }
+
+ handleChangeInput = (event) => {
+  this.setState({ userName: event.target.value });
+ }
+
+ login = () => {
+   this.setState({login: true})
+ }
 
   render() {
-
-    return (
+    let x = "Please enter Username";
+    // if(this.state.userName.length > 0 && this.state.login) 
+      x = <Player userName = {this.state.userName}></Player>;
+    
+      return (
       <div className = "App">
+        <input value={this.state.userName} onChange={this.handleChangeInput} onKeyUp={this.handleKeyUp}></input>
+        <button onClick={this.login}>Login</button>
         <header className = "App-header">
-          <Player></Player>
+          {x}
         </header>
       </div>
     );
   }
-
+ 
 }
 
 export default App;
